cc369768aa6f5cdeae8dc2d512d9b519e9ddbd3b
Asif Iqbal <asif.iq.024@gmail.com>
2023-11-30 22:16:54 +0600

    dependencies

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -28,12 +28,16 @@ class HomePage extends StatelessWidget {
           children: <Widget>[
             UserAccountsDrawerHeader(
               accountName:
-                  Text(authController.currentUser.value!.userName!.toString()),
+                  Text(authController.currentUser.value!.userName.toString()),
               accountEmail:
-                  Text(authController.currentUser.value!.email!.toString()),
+                  Text(authController.currentUser.value!.email.toString()),
               currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    "http://taskit.pockethost.io/${authController.currentUser.value!.avatar!.toString()}"),
+                backgroundImage: authController.currentUser.value!.avatar ==
+                        null
+                    ? const NetworkImage(
+                        "https://th.bing.com/th/id/R.fa0ca630a6a3de8e33e03a009e406acd?rik=UOMXfynJ2FEiVw&riu=http%3a%2f%2fwww.clker.com%2fcliparts%2ff%2fa%2f0%2fc%2f1434020125875430376profile.png&ehk=73x7A%2fh2HgYZLT1q7b6vWMXl86IjYeDhub59EZ8hF14%3d&risl=&pid=ImgRaw&r=0")
+                    : NetworkImage(
+                        "http://taskit.pockethost.io/${authController.currentUser.value!.avatar.toString()}"),
               ),
             ),
             ListTile(
